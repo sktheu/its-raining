81ed1499b399f805d33fcc5965d488acc27567f7
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2023-02-02 19:00:45 -0300

    Player Movement & State Machine

---
+++ Its Raining/Assets/_Game/_Scripts/Entities/Player/PlayerMovement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using Unity.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Unity Access Fields
+    [Header("Movement:")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    [Header("Dash:")] 
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashInterval;
+    [SerializeField, ReadOnly] private bool canDash = true;
+    
+    // Components
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spr;
+    
+    // Move
+    private Vector2 _moveInput;
+    
+    // State
+    private PlayerStateMachine.States _curState;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _spr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _curState = PlayerStateMachine.GetState();
+        
+        GetMovementInput();
+        GetDashInput();
+        FlipSprite();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_curState == PlayerStateMachine.States.NotPlaying) return;
+        else if (_curState == PlayerStateMachine.States.Moving) ApplyMovement();
+        else if (_curState == PlayerStateMachine.States.Dashing) ApplyDash();
+    }
+
+    private void GetMovementInput()
+    {
+        if (_curState != PlayerStateMachine.States.Dashing)
+        {
+            _moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            if (_moveInput != Vector2.zero) PlayerStateMachine.SetState(PlayerStateMachine.States.Moving);
+        }
+    }
+
+    private Vector2 SetVelocity(Vector2 goal, Vector2 cur, float accel)
+    {
+        var move = Vector2.zero;
+        var speedDiffX = goal.x - cur.x;
+        var speedDiffY = goal.y - cur.y;
+
+        if (speedDiffX > accel) move.x = cur.x + accel;
+        else if (speedDiffX < -accel) move.x = cur.x - accel;
+        else move.x = goal.x;
+
+        if (speedDiffY > accel) move.y = cur.y + accel;
+        else if (speedDiffY < -accel) move.y = cur.y - accel;
+        else move.y = goal.y;
+        
+        return move;
+    }
+
+    private void ApplyMovement()
+    {
+        _rb.velocity = SetVelocity(maxSpeed * _moveInput, _rb.velocity, acceleration);
+        
+        if (_rb.velocity == Vector2.zero) PlayerStateMachine.SetState(PlayerStateMachine.States.Playing);
+    }
+
+    private void FlipSprite()
+    {
+        if (_moveInput.x == -1f) _spr.flipX = true;
+        else if (_moveInput.x == 1f) _spr.flipX = false;
+    }
+
+    private void GetDashInput()
+    {
+        if (Input.GetButtonDown("Dash") && canDash && _moveInput != Vector2.zero)
+        {
+            canDash = false;
+            PlayerStateMachine.SetState(PlayerStateMachine.States.Dashing);
+            StartCoroutine(StopDash(dashTime));
+            StartCoroutine(DashInterval(dashInterval));
+        }
+    }
+
+    private void ApplyDash()
+    {
+        _rb.velocity = Vector2.zero;
+        _rb.velocity = _moveInput * dashSpeed;
+    }
+
+    private IEnumerator StopDash(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _rb.velocity = Vector2.zero;
+        PlayerStateMachine.SetState(PlayerStateMachine.States.Playing);
+    }
+
+    private IEnumerator DashInterval(float time)
+    {
+        yield return new WaitForSeconds(time);
+        canDash = true;
+    }
+}
---
+++ Its Raining/Assets/_Game/_Scripts/Entities/Player/PlayerStateMachine.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    public static PlayerStateMachine stateManager;
+    
+    [SerializeField, ReadOnly] private States currentState = States.Playing;
+    
+    public enum States
+    {
+        NotPlaying,
+        Playing,
+        Moving,
+        Dashing,
+    };
+
+    private void Awake()
+    {
+        stateManager = this;
+    }
+
+    public static States GetState()
+    {
+        return stateManager.currentState;
+    }
+
+    public static void SetState(States state)
+    {
+        stateManager.currentState = state;
+    }
+}
